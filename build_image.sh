8e305cbc83f324553a5877c3deabaec1da9bd3f3
Yu-Ren Zhang <e8035669@gmail.com>
2024-08-12 20:45:37 +0800

    update centos mirror

--- build_image.sh
+++ build_image.sh
@@ -2,5 +2,5 @@
 
 docker build --rm -t e8035669/linux-anvil-armv7l -f linux-anvil-armv7l/Dockerfile .
 
-docker tag e8035669/linux-anvil-armv7l:latest e8035669/linux-anvil-armv7l:1.0.6
+docker tag e8035669/linux-anvil-armv7l:latest e8035669/linux-anvil-armv7l:1.0.7
 
